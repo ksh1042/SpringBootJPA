639dd6c2864b46df151fa0b04bd81e37909176c7
= <roman14@naver.com>
2022-07-13 22:50:39 +0900

    Member 엔티티 수정

--- src/main/java/com/roman14/springbootjpa/entity/Member.java
+++ src/main/java/com/roman14/springbootjpa/entity/Member.java
@@ -1,16 +1,22 @@
 package com.roman14.springbootjpa.entity;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
-@Getter
 @Entity
+@Getter @Setter
 public class Member
 {
   @Id
   @GeneratedValue
   private Long id;
+
+  private String name;
+
+  private LocalDateTime addTime;
 }
