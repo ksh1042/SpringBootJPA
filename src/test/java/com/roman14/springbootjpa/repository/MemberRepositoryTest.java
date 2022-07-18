949387d610fbe7f3f72d95be84f6ccacddd1f208
= <roman14@naver.com>
2022-07-18 22:55:57 +0900

    테스트 트랜잭션 롤백 비활성화

--- src/test/java/com/roman14/springbootjpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/roman14/springbootjpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,6 +35,7 @@ class MemberRepositoryTest
 
   @Test
   @Transactional
+  @Rollback(false)
   public void save() throws Exception
   {
     // given
