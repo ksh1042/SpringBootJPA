81efcba7a782a5b389ce8fedba9777f5aa3b3b0c
= <roman14@naver.com>
2022-11-01 20:50:07 +0900

    홈 화면 추가

---
+++ src/main/java/com/roman14/springbootjpa/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.roman14.springbootjpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class HomeController
+{
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String home()
+  {
+    log.info("home called");
+    return "home";
+  }
+}
