646d379544fb36d76cb6ef7a2f54590f47f35bda
= <roman14@naver.com>
2022-11-12 03:55:56 +0900

    연관관계 LAZY로 설정된 엔티티의 null 응답을 위한 hibernate5module 추가

--- src/main/java/com/roman14/springbootjpa/SpringBootJpaApplication.java
+++ src/main/java/com/roman14/springbootjpa/SpringBootJpaApplication.java
@@ -12,4 +12,12 @@ public class SpringBootJpaApplication
     SpringApplication.run(SpringBootJpaApplication.class, args);
   }
 
+  /**
+   * Hibernate5Module을 빈에 등록 해주면, 지연로딩 객체 즉 LAZY 타입을 자동으로 NULL 값으로 출력해준다.
+   */
+  /*@Bean
+  Hibernate5Module hibernate5Module()
+  {
+    return new Hibernate5Module();
+  }*/
 }
